b7a1ce810ed2f2f1056c17e115f324a1d511f961
yeaunome <101441716+yeaunome@users.noreply.github.com>
2026-02-17 20:18:37 -0600

    Create Altoholic_LocalSync_CrossFaction.lua file

---
+++ Altoholic_LocalSync_CrossFaction/Altoholic_LocalSync_CrossFaction.lua
@@ -0,0 +1,10 @@
+## Interface\AddOns\Altoholic_LocalSync_CrossFaction\Altoholic_LocalSync_CrossFaction.lua
+-- This is a placeholder for the Altoholic_LocalSync_CrossFaction Lua file.
+
+function Altoholic_LocalSync_CrossFaction:Init()
+    -- Initialization code goes here
+end
+
+function Altoholic_LocalSync_CrossFaction:OnEvent(event, arg1)
+    -- Event handling code goes here
+end
